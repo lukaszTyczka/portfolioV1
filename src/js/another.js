58335d75ba088c42b599682eb8007bbe360b2b94
Łukasz Tyczka <lukasz@tyczka.net>
2021-01-19 22:08:39 +0100

    repair gear center

--- src/js/another.js
+++ src/js/another.js
@@ -1,1 +1,9 @@
 console.log('hello another');
+
+var gear = document.querySelector('.gear');
+var bbox = gear.getBBox();
+
+var x = bbox.x + bbox.width / 2;
+var y = bbox.y + bbox.height / 2;
+
+console.log(x, y);
